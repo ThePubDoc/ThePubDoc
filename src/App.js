8994961e451b04b93fff3993c4d52879bef18ea5
Aayush <aayush9152@gmail.com>
2020-07-19 00:34:31 +0530

    theme added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ResetStyle from './GlobalStyles/Reset.style';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './GlobalStyles/Theme';
 
 const App = () => {
-  return <h1>Hello</h1>;
+  // const [darkTheme, setDarkTheme] = useState(false);
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <ResetStyle />
+      <h1>Hello</h1>
+    </ThemeProvider>
+  );
 };
 
 export default App;
